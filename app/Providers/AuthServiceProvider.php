ae843d13b011e8e2906ed98eee6339a680212c4e
boryeira <jmanuel.jorquera@gmail.com>
2019-01-07 17:45:17 -0300

    refrwessh token

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -30,7 +30,7 @@ class AuthServiceProvider extends ServiceProvider
 
         Passport::routes();
 
-        Passport::tokensExpireIn(now()->addMinutes(1));
+        Passport::tokensExpireIn(now()->addDays(1));
 
         Passport::refreshTokensExpireIn(now()->addDays(30));
     }
