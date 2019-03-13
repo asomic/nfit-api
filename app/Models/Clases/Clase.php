2dfcbfe39efef1ace0dba8946b9121a9b8cc6d7c
boryeira <jmanuel.jorquera@gmail.com>
2019-03-13 11:48:10 -0300

    reservation bug 2

--- app/Models/Clases/Clase.php
+++ app/Models/Clases/Clase.php
@@ -54,14 +54,14 @@ class Clase extends Model
     public function auth_can_reserve()
     {
       //maximo de 3 usuarios de prueba
-      $clases = Clase::where('date', $this->$date)->get();
+      $clases = Clase::where('date', $this->date)->get();
       foreach ($clases as $clase) {
           $reservations = Reservation::where('user_id', Auth::id())->where('clase_id', $clase->id)->get();
           if (count($reservations) != 0) {
               return false;
           }
       }
-    
+
       if(Auth::user()->status_user_id == 3){
 
         $pruebaCount = 0;
