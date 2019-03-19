2fef6bb4d834ccf3c27363982cabe3e0a5fa8a73
RaulEbc <raulberrios8@gmail.com>
2019-03-19 16:02:53 -0300

    Revert "add supply to reservs"
    This reverts commit e54671a0e59ded087a1cdf6bcc2412838370fd2e.

--- app/Http/Controllers/Clases/ClaseController.php
+++ app/Http/Controllers/Clases/ClaseController.php
@@ -119,16 +119,8 @@ class ClaseController extends ApiController
       $reservation->user_id = Auth::user()->id;
       $reservation->clase_id = $clase->id;
       $reservation->by_god = false;
-      if($planuser){
-        $reservation->plan_user_id = $planuser->id;
-        if (!in_array($planuser->plan->id, $clase->block->plans->pluck('id')->toArray())) {
-          return $this->errorResponse('Tu plan no te deja tomar esta clase', 400);
-        }
-      }
+      if($planuser) {$reservation->plan_user_id = $planuser->id;}
       $reservation->reservation_status_id = 1 ;
-
-      
-      // dd('hola');
       if($reservation->save())
       {
         $planuser->counter = $planuser->counter - 1;
