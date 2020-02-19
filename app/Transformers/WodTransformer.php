e47522de7b9db8cbaf1cab9306ba7962e7bc5074
boryeira <jmanuel.jorquera@gmail.com>
2020-02-19 15:07:59 -0300

    wod transformer update

--- app/Transformers/WodTransformer.php
+++ app/Transformers/WodTransformer.php
@@ -15,18 +15,12 @@ class WodTransformer extends TransformerAbstract
      */
     public function transform(Wod $wod)
     {
-        $reservationHas = false;
-        $reservation = Auth::User()->clases()->where('date',today())->where('clase_type_id',$wod->clase_type_id)->first();
-        $todayReservation = [];
-
-    
-        
 
         if(count($wod->stages)>0)
         {
             $hasStages = true ;
             $stages = $wod->stages;
-            $featuredStage = '';
+            $featuredStage = $stages[0]->description;
             foreach($stages as $stage )
             {
                 if($stage->stage_type->featured == 1 )
@@ -41,40 +35,18 @@ class WodTransformer extends TransformerAbstract
         }
 
 
-
-        if($reservation){
-          $reservationHas = true;
-          $todayReservation = [
-            'id' => (int)$reservation->id,
-            'status' => (int)$reservation->pivot->reservation_status_id,
-            'start' => (string)date('H:i', strtotime($reservation->start_at)),
-            'end' => (string)date('H:i', strtotime($reservation->finish_at)),
-            'href' => (string)route('clases.show', ['clase' => $reservation->id]),
-          ];
-        }
-
         return [
             'id' => (int)$wod->id,
-            'claseTypeId' => (int)$wod->clase_type_id,
-            'claseType' => (string)$wod->claseType->clase_type,
-            'fecha' => (string)$wod->date,
             'day' => (string)$wod->date->format('d'),
             'month' => (string)strtoupper ( $wod->date->formatLocalized('%b')) ,
-            'dateHuman' =>  (string)ucfirst($wod->date->formatLocalized('%A %d')).' de '.ucfirst($wod->date->formatLocalized('%B')) ,
             'year' => (string)$wod->date->formatLocalized('%Y'),
+            'dateHuman' =>  (string)ucfirst($wod->date->formatLocalized('%A %d')).' de '.ucfirst($wod->date->formatLocalized('%B')) ,
             'rels' => [
-                'self' => [
-                    'href' => '',
-                ],
                 'claseType' => [
                     'id' => (int)$wod->clase_type_id,
                     'name' => (string)$wod->claseType->clase_type,
                     'icon' => (string) url('/').'/icon/clases/'.$wod->claseType->icon,
                     'iconWhite' => (string) url('/').'/icon/clases/'.$wod->claseType->icon_white,
-                ],
-                'auth' => [
-                    'reservationHas' => (boolean)$reservationHas,
-                    'todayReservation' => $todayReservation,
                 ],
                 'stages' => [
                     'has' => (boolean)$hasStages,
