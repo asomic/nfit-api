6a2f31a4aaf9549fc0a4f982234fefa11b46d158
jose manuel jorquera <tiuditcon.cl>
2020-11-12 19:41:07 -0300

    up

---
+++ app/Http/Controllers/Videos/VideoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Videos;
+
+use App\Http\Controllers\ApiController;
+use App\Models\Videos\Video;
+use Auth;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+
+class VideoController extends ApiController
+{
+
+    public function index()
+    {
+        $videos = Videos::all();
+        return $this->showAll($videos);
+    }
+
+
+}
+
---
+++ app/Transformers/VideoTransformer.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Transformers;
+
+
+use App\Models\Videos\VIdeo;
+use League\Fractal\TransformerAbstract;
+use Carbon\Carbon;
+use Auth;
+
+class ClaseTransformer extends TransformerAbstract
+{
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Video $video)
+    {
+
+        return [
+            'id' => (int)$video->id,
+
+        ];
+    }
+
+    /**
+     *  Undocumented function
+     *
+     *  @param   [type] $index
+     * 
+     *  @return  array|null
+     */
+    public static function originalAttribute($index)
+    {
+        $attributes = [
+            'id' => 'id',
+
+        ];
+
+        return isset($attributes[$index]) ? $attributes[$index] : null;
+    }
+
+    /**
+     *  Undocumented function
+     *
+     *  @param  [type] $index
+     *  
+     *  @return  array|null
+     */
+    public static function transformedAttribute($index)
+    {
+        $attributes = [
+            'id' => 'id',
+
+        ];
+
+        return isset($attributes[$index]) ? $attributes[$index] : null;
+    }
+}
