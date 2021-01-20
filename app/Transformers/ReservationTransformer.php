696f3bb162921f3136b36ebda007882814a684c7
Raulebc <raulberrios8@gmail.com>
2021-01-20 15:10:12 -0300

    =

--- app/Transformers/ReservationTransformer.php
+++ app/Transformers/ReservationTransformer.php
@@ -21,25 +21,25 @@ class ReservationTransformer extends TransformerAbstract
             'rels' => [
                 'clase' => [
                     'id' => (int)$reservation->clase_id,
-                    'start' => (string)$reservation->clase->start_at,
-                    'end' => (string)$reservation->clase->finish_at,
-                    'date' => (string)$reservation->clase->date,
+                    'start' => (string) $reservation->clase->start_at,
+                    'end' => (string) $reservation->clase->finish_at,
+                    'date' => (string) $reservation->clase->date,
                     'dateHuman' => (string) ucfirst(strftime('%A %d de %B, %Y', $reservation->clase->date->timestamp)),
                 //   'dateHuman' => (string) ucfirst($reservation->clase->date->formatLocalized('%A %d')).' de '.ucfirst($reservation->clase->date->formatLocalized('%B, %Y')) ,
                     'day' => (string) ucfirst(strftime('%A %d', $reservation->clase->date->timestamp)),
                 //   'day' => (string)ucfirst($reservation->clase->date->formatLocalized('%A %d')),
                     'month' =>(string) ucfirst($reservation->clase->date->formatLocalized('%B')),
                     'year' =>(string) $reservation->clase->date->formatLocalized('%Y'),
-                    'href' => (string)route('clases.show', ['clase' => (int)$reservation->clase_id]),
+                    'href' => (string) route('clases.show', ['clase' => (int) $reservation->clase_id]),
                     'users' => [
-                        'count' => (int)count($reservation->clase->users),
+                        'count' => (int) count($reservation->clase->users),
                         // 'prueba_count' => (int)count($reservation->clase->pruebaUsersCount()),
                         'href' => (string)route('clases.users', ['clase' => (int)$reservation->clase_id]),
                         ],
                     ],
                     'wod' => [
                         'id' => (int)$reservation->clase->wod_id,
-                        'href' => route('wods.show', ['wod' => (int)$reservation->clase->wod_id])
+                        'href' => route('wods.show', ['wod' => (int) $reservation->clase->wod_id])
                     ],
                     'user' => [
                         'id' => (int)$reservation->user->id,
